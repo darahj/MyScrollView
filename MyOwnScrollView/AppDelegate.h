a103728352fb1c17563e6286034f4e9d0fc38b34
Darah <darah.joseph@gmail.com>
2015-08-17 08:48:04 -0400

    Initial Commit

---
+++ MyOwnScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyOwnScrollView
+//
+//  Created by Darah on 2015-08-17.
+//  Copyright (c) 2015 Darah Joseph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyOwnScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyOwnScrollView
+//
+//  Created by Darah on 2015-08-17.
+//  Copyright (c) 2015 Darah Joseph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
